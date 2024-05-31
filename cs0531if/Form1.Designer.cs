36e3f591536419cce8c3aa57e9a5eeee8a4a8281
YuTanaka <tanakaedu@users.noreply.github.com>
2024-05-31 16:09:16 +0900

    演習8-4

--- cs0531if/Form1.Designer.cs
+++ cs0531if/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Yu Gothic UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(74, 311);
+            label1.Location = new Point(456, 320);
             label1.Name = "label1";
             label1.Size = new Size(105, 45);
             label1.TabIndex = 0;
--- cs0531if/Form1.cs
+++ cs0531if/Form1.cs
@@ -14,6 +14,23 @@ namespace cs0531if
         {
             label1.Left += vx;
             label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+            if (label1.Top < 0)
+            {
+                vy = 10;
+            }
+            if (label1.Left > 456)
+            {
+                vx = -10;
+            }
+            if (label1.Top > 320)
+            {
+                vy = -10;
+            }
         }
     }
 }
